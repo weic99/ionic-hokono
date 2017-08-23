1e25cd0afd14101e6ef66a7cc865baa033e93fcd
Wei Chen <tengwei1@gmail.com>
2017-08-22 22:02:03 -0400

    tabs init

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
-
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = 'HomePage';
+  tab2Root = 'SearchPage';
+  tab3Root = 'CameraPage';
+  tab4Root = 'FollowingPage';
+  tab5Root = 'ProfilePage';
 
   constructor() {
 
