0749e808863cfe34c57dc604af67f8d0716076c2
Wei Chen <tengwei1@gmail.com>
2017-08-23 00:37:48 -0400

    virtualscroll is broken,

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -25,26 +25,3 @@ export class ProfilePage {
   }
 
 }
-
-import {updateImgs} from "ionic-angular/components/content/content";
-import {ViewChild} from "@angular/core";
-import {Content} from "ionic-angular";
-
-@Component({
-  templateUrl: 'virtual-list.html',
-})
-export class VirtualListPage {
-  @ViewChild(Content) _content: Content;
-  ngAfterViewInit(){
-    if(this._content) {
-      this._content.imgsUpdate = () => {
-        if (this._content._scroll.initialized && this._content._imgs.length && this._content.isImgsUpdatable()) {
-          // reset cached bounds
-          this._content._imgs.forEach((img:Img)=>img._rect = null);
-          // use global position to calculate if an img is in the viewable area
-          updateImgs(this._content._imgs, this._content._cTop * -1, this._content.contentHeight, this._content.directionY, 1400, 400);
-        }
-      };
-    }
-  }
-}
