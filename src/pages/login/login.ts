f569c85170b0c0003706f9225f014f63fe1fa6a6
Wei Chen <tengwei1@gmail.com>
2017-08-24 19:32:42 -0400

    google sign in

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
 
+import { Platform } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
 
@@ -19,7 +20,8 @@ export class LoginPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private afAuth: AngularFireAuth
+    private afAuth: AngularFireAuth,
+    private platform: Platform
   ) {
     this.account = {
       email: '',
@@ -35,25 +37,20 @@ export class LoginPage {
     console.log(str);
 
     if(str === 'google') {
-      this.afAuth.auth
-      .signInWithPopup(new firebase.auth.GoogleAuthProvider())
-      .then(res => {
-        console.log(res);
-        this.navCtrl.setRoot(TabsPage);
-      })
-      .catch(err => {
-        console.log('login(google)', err);
-      });
-    } else if (str === 'facebook') {
-      this.afAuth.auth
-      .signInWithPopup(new firebase.auth.FacebookAuthProvider())
-      .then(res => {
-        console.log(res);
-        this.navCtrl.setRoot(TabsPage);
-      })
-      .catch(err => {
-        console.log('login(google)', err);
-      });
+      if (this.platform.is('cordova')) {
+
+      } else {
+        // web sign in, will not work
+        return this.afAuth.auth
+        .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+        .then(res => {
+          console.log(res);
+          this.navCtrl.setRoot(TabsPage);
+        })
+        .catch(err => {
+          console.log('login(google)', err);
+        });
+      }
     }
   }
 
