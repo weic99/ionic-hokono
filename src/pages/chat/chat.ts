ff0547175ed27fba81198c0bbcf5e951d36e7746
Wei Chen <tengwei1@gmail.com>
2017-08-30 19:49:02 -0400

    now scrolls to bottom on chat focus

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Content } from 'ionic-angular';
 
 import { User } from '../../models/user';
 import { Message } from '../../models/message'
@@ -12,6 +12,8 @@ import { Message } from '../../models/message'
 })
 export class ChatPage {
 
+  @ViewChild(Content) content: Content;
+
   friend = {} as User;
   user = {} as User;
   newMessage = {} as Message;
@@ -22,6 +24,34 @@ export class ChatPage {
     public navParams: NavParams
   ) {
     this.friend.displayName = 'Mario';
+
+    this.messages = [
+      {
+        sender: 'Mario',
+        receiver: '',
+        body: 'Hi'
+      },
+      {
+        sender: 'Mario',
+        receiver: '',
+        body: 'Hi'
+      },
+      {
+        sender: 'Mario',
+        receiver: '',
+        body: 'heyyyyyyyyyyyyyyyyy'
+      },
+      {
+        sender: 'Mario',
+        receiver: '',
+        body: 'heeeeeeelllllloooooooooooooooooooooooooooooooooo'
+      },
+      {
+        sender: 'Mario',
+        receiver: '',
+        body: 'heeeeeeelllllloooooooooooooooooooooooooooooooooo'
+      },
+    ];
   }
 
   ionViewDidLoad() {
@@ -35,4 +65,8 @@ export class ChatPage {
     e.target.reset();
   }
 
+  scrollToBottom() {
+    console.log('focus');
+    this.content.scrollToBottom();
+  }
 }
