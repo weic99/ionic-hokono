4f34820575afed684e4d3d57f7d37d6e97eef1a5
Wei Chen <tengwei1@gmail.com>
2017-08-30 15:08:59 -0400

    refactored

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ChatPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-chat',
@@ -19,7 +12,7 @@ export class ChatPage {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ChatPage');
+
   }
 
 }
